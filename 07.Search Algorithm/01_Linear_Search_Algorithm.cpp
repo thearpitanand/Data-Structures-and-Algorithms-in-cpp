8f1848c684cc843ee12b57e5c456538f1d56d598
Arpit Anand <arpitssanand@gmail.com>
2021-09-23 23:35:34 +0530

    Add Searching Algorithm

---
+++ 07.Search Algorithm/01_Linear_Search_Algorithm.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int a[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] == key)
+        {
+            return (i + 1);
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int a[] = {9, 8, 5, 4, 2, 1};
+    int index = linearSearch(a, 6, 25);
+    if (index != -1)
+    {
+        cout << "Value found at position: " << index << endl;
+    }
+    else
+    {
+        cout << "Value not found" << endl;
+    }
+
+    return 0;
+}
